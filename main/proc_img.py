63c70ffd5c76a8847f280109a9946607bc5cb85d
Lucas <lucsppaiva@gmail.com>
2019-10-08 21:01:05 -0400

    link do cod

--- main/proc_img.py
+++ main/proc_img.py
@@ -916,6 +916,7 @@ def play_video(video_file):
 def tester():
     # esse é um trecho do meu tester, que eu tava usando para fazer a similaridade entres os frames
     # play_video("../videos/bf42.mp4")
+    #extraido de : https://www.pyimagesearch.com/2014/07/14/3-ways-compare-histograms-using-opencv-python/
     '''frames = vito.load_video("../videos/bf42.mp4")
     i = 0
     for f in frames[:25]:
